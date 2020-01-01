e7efad18751ba644752e10c7b213d655bb845239
양예은 <yye7926@naver.com>
2020-01-01 17:13:52 +0900

    NewsView에 actions와 mutations적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,31 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchNewsList } from '../api/index.js';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     news: []
+  },
+  mutations: {  // mutations에서 state로
+    SET_NEWS(state, news) { // news = response.data
+      state.news = news;
+    }
+  },
+  actions: {  // api꺼내와서 mutations으로
+    FETCH_NEWS(context) {
+      fetchNewsList()
+      .then(response => {
+        console.log(response);
+        context.commit('SET_NEWS', response.data); 
+        // commit : mutations에 데이터 넘김
+        // SET_NEWS를 시행, response.data를 넘김
+
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
   }
 });
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,29 +1,25 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user.id">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news" :key="user.id">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
+// import { fetchNewsList } from '../api/index.js';
 
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    console.log('호출 전: ', this);
+    this.$store.dispatch('FETCH_NEWS');
+  //   console.log('호출 전: ', this);
 
-    fetchNewsList()
-    .then(response => {
-      console.log('호출 후: ', this);
-      this.users = response.data;
-    })
-    .catch(function(error) {
-      console.log(error)
-    })
+  //   fetchNewsList()
+  //   .then(response => {
+  //     console.log('호출 후: ', this);
+  //     this.users = response.data;
+  //   })
+  //   .catch(function(error) {
+  //     console.log(error)
+  //   })
   },
 }
 </script>
