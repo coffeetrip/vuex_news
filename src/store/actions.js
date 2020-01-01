a281c8f77b7196c9b243392ca8bc3742060b6182
양예은 <yye7926@naver.com>
2020-01-01 18:01:54 +0900

    스토어 속성 모듈화 수정

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,37 +6,35 @@ import {
 
 export default {
   FETCH_NEWS(context) {
-      fetchNewsList()
-        .then(response => {
-          console.log(response);
-          context.commit('SET_NEWS', response.data);
-          // commit : mutations에 데이터 넘김
-          // SET_NEWS를 시행, response.data를 넘김
-        })
-        .catch(error => {
-          console.log(error)
-        });
-    },
-    FETCH_JOBS({
-      commit
-    }) {
-      fetchJobsList()
-        .then(({
-          data
-        }) => {
-          commit('SET_JOBS', data);
-        })
-        .catch(error => console.log(error));
-    },
-    FETCH_ASK({
-      commit
-    }) {
-      fetchAskList()
-        .then(({
-          data
-        }) => {
-          commit('SET_ASK', data);
-        })
-        .catch(error => console.log(error));
+    fetchNewsList()
+      .then(response => {
+        console.log(response);
+        context.commit('SET_NEWS', response.data);
+        // commit : mutations에 데이터 넘김
+        // SET_NEWS를 시행, response.data를 넘김
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  },
+  FETCH_JOBS({ commit }) {
+    fetchJobsList()
+      .then(({
+        data
+      }) => {
+        commit('SET_JOBS', data);
+      })
+      .catch(error => console.log(error));
+  },
+  FETCH_ASK({
+    commit
+  }) {
+    fetchAskList()
+      .then(({
+        data
+      }) => {
+        commit('SET_ASK', data);
+      })
+      .catch(error => console.log(error));
     },
   }
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import mutations from './mutations.js';
-import actions from './mutations.js';
-
+import actions from './actions.js';
 
 Vue.use(Vuex);
 
