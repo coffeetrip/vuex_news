6cf59d4baa01dcff2ae5ea41f8610fb260b2275a
양예은 <yye7926@naver.com>
2020-01-02 12:33:49 +0900

    이벤트 버스를 이용한 스피너 컴포넌트 구현

---
+++ src/utiles/bus.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+// // bus.js
+// export const bus = new Vue();
+
+// //App.vue
+// import { bus } from './bus.js';
+
+
+// bus.js
+export default new Vue();
+
+// // App.vue
+// import Bus from './bus.js';  // 자유롭게 선언가능
