7c01dc59f79e3c4d7e4263e9f05789ea00d11883
양예은 <yye7926@naver.com>
2020-01-02 17:36:31 +0900

    라우터 네비게이션

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import JobsView from '../views/JobsView.vue'
 import AskView from '../views/AskView.vue'
 import ItemView from '../views/ItemView.vue'
 import UserView from '../views/UserView.vue'
+import bus from '../utiles/bus.js';
+import { store } from '../store/index.js';
 // import createListView from '../views/CreateListView.js'
 
 Vue.use(VueRouter)
@@ -20,6 +22,21 @@ export const router = new VueRouter({
       name: 'news',
       component: NewsView,
       // component: createListView('NewsView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        // setTimeout(() => {
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            console.log('fetched');
+            bus.$emit('end:spinner');
+            next();
+          })
+          .catch((error) => console.log(error));
+         // }, 1000);
+        // console.log('to', to);
+        // console.log('from', from);
+        // console.log('next', next);
+      }
     },
     {
       path: '/ask',
